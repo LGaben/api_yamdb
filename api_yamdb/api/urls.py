7ada1473538ef6dce73e36a986a116e2162ae28c
LGaben <120731463+LGaben@users.noreply.github.com>
2023-07-01 13:51:00 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,19 +6,17 @@ from .views import (TitleViewSet,
                     GenreViewSet,
                     UserViewSet,
                     SignUpViewSet,
-                    TokenViewSet) 
+                    TokenViewSet)
 
-app_name = 'api'
+router_v1 = DefaultRouter()
 
-v1_router = DefaultRouter()
-
-v1_router.register(r'titles', TitleViewSet, basename='titles')
-v1_router.register(r'categories', CategoryViewSet, basename='categories')
-v1_router.register(r'genres', GenreViewSet, basename='genres')
-v1_router.register(r'users', UserViewSet, basename='users')
+router_v1.register(r'titles', TitleViewSet, basename='titles')
+router_v1.register(r'categories', CategoryViewSet, basename='categories')
+router_v1.register(r'genres', GenreViewSet, basename='genres')
+router_v1.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(v1_router.urls)),
+    path('', include(router_v1.urls)),
     #path('v1/auth/signup/', SignUpViewSet.as_view(), name='signup'),
     #path('v1/auth/token/', TokenViewSet.as_view(), name='get_token'), 
 ]
