fae700f6e80127cf3fefac4a51a2587f17ede44a
LGaben <120731463+LGaben@users.noreply.github.com>
2023-07-04 21:14:30 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -31,7 +31,7 @@ router_v1.register(
 )
 
 urlpatterns = [
-    path('v1', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', SignUpViewSet.as_view(), name='signup'),
     # path('v1/auth/token/', TokenViewSet.as_view(), name='get_token'),
 ]
