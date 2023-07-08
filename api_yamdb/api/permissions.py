cdee89c27ea910832fbab6ba85a8284ec4652237
LGaben <120731463+LGaben@users.noreply.github.com>
2023-07-08 21:04:47 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,19 +12,12 @@ class IsAdminOrOnlyRead(permissions.BasePermission):
 class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     message = 'Изменить контент может только автор.'
 
-    def has_object_permission(self, request, view, obj): 
-        return ( 
-            request.method in permissions.SAFE_METHODS 
-            or request.user.is_admin 
-            or request.user.is_moderator 
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
             or request.user == obj.author)
-
-
-class IsModerator(permissions.IsAuthenticatedOrReadOnly):
-    message = 'Изменить контент может только админ или модератор.'
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_moderator
 
 
 class IsAdmin(permissions.BasePermission):
