8d13d7becdf25fd818c3ad5a810e6637d51a7cf9
Иван Филин <zobiemc@gmail.com>
2023-07-09 00:44:25 +0300

    kurwa ya perdole

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -20,6 +20,27 @@ class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
             or request.user == obj.author)
 
 
+class IsOwner(permissions.IsAuthenticatedOrReadOnly):
+    message = 'Изменить контент может только админ или модератор.'
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_authenticated
+            and request.user == obj.author)
+
+class ReadOnly(permissions.BasePermission):
+    message = 'Изменить контент может только админ или модератор.'
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+class IsModerator(permissions.BasePermission):
+    message = 'Изменить контент может только админ или модератор.'
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_moderator
+
+
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
