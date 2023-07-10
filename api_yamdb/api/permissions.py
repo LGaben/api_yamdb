cb77257031b0141cced4d7706f275ee22751f59e
Иван Филин <zobiemc@gmail.com>
2023-07-10 19:52:36 +0300

    Сдано

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,43 +8,28 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             or (request.user.is_authenticated and request.user.is_admin)
         )
 
-# В версии djangorestframework-3.14.0 данные пермишены проходят тесты
-# На версии djangorestframework-3.12.4 отказывается ((
-# поэтому оставили так. Правильно ли мы их сделали?
-# После ревью комментарии удалю.
 
-
-class IsOwnerAdminModeratorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
-    message = 'Изменить контент может только автор.'
+class IsOwner(permissions.IsAuthenticatedOrReadOnly):
+    message = 'Изменить контент может только админ или модератор.'
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_admin
-            or request.user.is_moderator
-            or request.user == obj.author)
-
-# class IsOwner(permissions.IsAuthenticatedOrReadOnly):
-#     message = 'Изменить контент может только админ или модератор.'
-
-#     def has_object_permission(self, request, view, obj):
-#         return (
-#             request.user.is_authenticated
-#             and request.user == obj.author)
+            request.user.is_authenticated
+            and request.user == obj.author)
 
 
-# class ReadOnly(permissions.BasePermission):
-#     message = 'Изменить контент может только админ или модератор.'
+class ReadOnly(permissions.BasePermission):
+    message = 'Изменить контент может только админ или модератор.'
 
-#     def has_permission(self, request, view):
-#         return request.method in permissions.SAFE_METHODS
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
 
 
-# class IsModerator(permissions.BasePermission):
-#     message = 'Изменить контент может только админ или модератор.'
+class IsModerator(permissions.BasePermission):
+    message = 'Изменить контент может только админ или модератор.'
 
-#     def has_permission(self, request, view):
-#         return request.user.is_authenticated and request.user.is_moderator
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_moderator
 
 
 class IsAdmin(permissions.BasePermission):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -39,9 +39,11 @@ from .serializers import (
 )
 from .mixins import ListCreateDeleteViewSet
 from .permissions import (
-    IsOwnerAdminModeratorOrReadOnly,
+    IsOwner,
+    IsModerator,
     IsAdminOrReadOnly,
-    IsAdmin
+    IsAdmin,
+    ReadOnly
 )
 
 
@@ -159,7 +161,7 @@ class TokenViewSet(views.APIView):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (IsOwnerAdminModeratorOrReadOnly,)
+    permission_classes = [ReadOnly | IsOwner | IsModerator | IsAdmin]
 
     def get_title(self):
         return get_object_or_404(Title, pk=self.kwargs.get('title_id'))
@@ -175,7 +177,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsOwnerAdminModeratorOrReadOnly,)
+    permission_classes = [ReadOnly | IsOwner | IsModerator | IsAdmin]
 
     def get_review(self):
         title_id = self.kwargs.get('title_id')
